4725bbbc25ed7e88798ca53a8218e0c13a4d2f5b
Michael H W <rift.gaming.info@gmail.com>
2014-09-24 16:10:51 +0100

    Working on two machines

--- Lab1 - Winsock Setup/Lab1 - Winsock Setup/main.cpp
+++ Lab1 - Winsock Setup/Lab1 - Winsock Setup/main.cpp
@@ -28,25 +28,32 @@ void  playerA(Net* n){
 	char* ip = "127.0.0.1";
 	n->setupUDP(28000, ip);
 	//int m = n->receiveData("Player A");
-	cout<< n->portNum << " "<< n->getSenderIP();
+	cout<< n->portNum << " "<< n->getSenderIP()<<endl;
 
 	n->sendData(ip,28001,"Player A Message");
 	//n->receiveData(message);
-}
-
-void playerB(Net* n){
-	char* ip = "127.0.0.1";
-	n->setupUDP(28001, ip);
-	//int m = n->receiveData("Player B");
-	cout<< n->portNum<< " "<< n->getSenderIP();
-	//n->sendData(ip,28000,"Player B Message");
 	while(true){
 		 n->receiveData(message);
 		if(message[0] != '\0')
 		{
-			
-			 cout << message;
+			 cout << message<<endl;
+			 break;
 		}
 	}
-	
+}
+
+void playerB(Net* n){
+	char* ip = "149.153.102.45";
+	n->setupUDP(28001, ip);
+	//int m = n->receiveData("Player B");
+	cout<< n->portNum<< " "<< n->getSenderIP()<<endl;
+	n->sendData("149.153.102.32",28000,"Wow Message");
+	while(true){
+		 n->receiveData(message);
+		if(message[0] != '\0')
+		{
+			 cout << message<<endl;
+			 break;
+		}
+	}
 }
